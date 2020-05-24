f7180e9f0b0a863a2f40fb121a0a7e42ab37574d
csteitz01 <csteitz01@bellarmine.edu>
2020-05-24 01:08:12 -0400

    Added player movement, jumping, and added hourglass picture for pickup

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 200f;
+    Rigidbody2D myRigidBody2D;
+    Collider2D myCollider2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+        myCollider2D = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        Jump();
+    }
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody2D.velocity.y);
+        myRigidBody2D.velocity = playerVelocity;
+    }
+
+    private void Jump()
+    {
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidBody2D.velocity = jumpVelocityToAdd;
+        }
+    }
+
+}
