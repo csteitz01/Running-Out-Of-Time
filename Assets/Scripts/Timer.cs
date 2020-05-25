4a0e2b07f356e9c15853f9e5254c2ac887231468
csteitz01 <csteitz01@bellarmine.edu>
2020-05-25 00:10:03 -0400

    Added a timer, and added scenes for a menu and 2 other levels

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TextMeshProUGUI textMesh;
+    public float secondsLeft = 30;
+    public bool takingAway = false;
+
+    private void Start()
     {
-        
+        textMesh.GetComponent<TextMeshProUGUI>().text = "" + secondsLeft;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        secondsLeft -= 1 * Time.deltaTime;
+        textMesh.GetComponent<TextMeshProUGUI>().text = secondsLeft.ToString("0");
+
+        if (secondsLeft <= 0)
+        {
+            secondsLeft = 0;
+        }
     }
+
+    //IEnumerator TimerTake()
+    //{
+    //    takingAway = true;
+    //    yield return new WaitForSeconds(1);
+    //    secondsLeft -= 1;
+        
+    //    takingAway = false;
+    //}
+
 }
