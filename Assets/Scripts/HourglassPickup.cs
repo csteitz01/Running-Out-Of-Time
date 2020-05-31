c13a98543a9b3b3eb7fb31c51ab249b67fa5c34e
csteitz01 <csteitz01@bellarmine.edu>
2020-05-31 13:53:14 -0400

    Added music and sound for pickups, changed the buttons to look better

--- Assets/Scripts/HourglassPickup.cs
+++ Assets/Scripts/HourglassPickup.cs
@@ -7,9 +7,11 @@ public class HourglassPickup : MonoBehaviour
 
     [SerializeField] int timeToAdd = 2;
     [SerializeField] int backRotation = 24;
+    [SerializeField] AudioClip hourglassPickupSFX;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        AudioSource.PlayClipAtPoint(hourglassPickupSFX, Camera.main.transform.position);
         FindObjectOfType<Timer>().TimeAdd(timeToAdd);
         FindObjectOfType<ClockHand>().RotateBack(backRotation);
         Destroy(gameObject);
